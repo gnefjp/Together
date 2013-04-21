a46f7e3bf2e8024175508267a11111fe8d9aa67f
308147344@qq.com <B15-726c>
2013-04-22 02:02:58 +0800

    提交文件。。。
    提交文件。。。

---
+++ Together/Codes/Common/TestView.m
@@ -0,0 +1,31 @@
+//
+//  TestView.m
+//  Together
+//
+//  Created by APPLE on 13-4-22.
+//  Copyright (c) 2013年 GMET. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
