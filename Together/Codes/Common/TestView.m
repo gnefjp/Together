c30c0601bfd33618680293345c86baf719551757
Gnef_jp <apple@Gnef-jp.local>
2013-04-22 02:22:37 +0800

    Testttt

--- Together/Codes/Common/TestView.m
+++ Together/Codes/Common/TestView.m
@@ -25,7 +25,7 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    // Drawing codesdfsadfsadfsadfsdfsdafsadf
 }
 */
 
