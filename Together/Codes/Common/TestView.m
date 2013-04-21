e216067f2be1e2ea36bbd8a78d613f337d28112b
308147344@qq.com <B15-726c>
2013-04-22 02:24:16 +0800

    LL

--- Together/Codes/Common/TestView.m
+++ Together/Codes/Common/TestView.m
@@ -15,7 +15,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        NSLog(@"ssggtttttt");
+        NSLog(@"ssggttttttLL");
     }
     return self;
 }
