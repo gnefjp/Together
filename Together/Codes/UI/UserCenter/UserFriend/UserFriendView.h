acd1beab674d289277be45b0621e6eb71ccfd49d
308147344@qq.com <B15-726c>
2013-05-09 21:22:44 +0800

    网络卡，重提交
    网络卡，重提交

---
+++ Together/Codes/UI/UserCenter/UserFriend/UserFriendView.h
@@ -0,0 +1,17 @@
+//
+//  UserFriendView.h
+//  Together
+//
+//  Created by APPLE on 13-5-9.
+//  Copyright (c) 2013年 GMET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserFriendView : UIView
+{
+    
+}
+
+
+@end
---
+++ Together/Codes/UI/UserCenter/UserFriend/UserFriendView.m
@@ -0,0 +1,31 @@
+//
+//  UserFriendView.m
+//  Together
+//
+//  Created by APPLE on 13-5-9.
+//  Copyright (c) 2013年 GMET. All rights reserved.
+//
+
+#import "UserFriendView.h"
+
+@implementation UserFriendView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
